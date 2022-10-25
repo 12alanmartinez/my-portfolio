0015ca1e742fa9f8a935bd0f5d15d3923cdf614f
alan marinez <alanmartinez319@yahoo.com>
2022-10-25 00:24:54 -0500

    done with the portfolio section of my website, and added images in. just need to figure out why one of them doesn't show up. next is testimonials.

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,5 +1,60 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.2.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.jpg'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+// DO NOT USE THE IMAGES IN PRODUCTION //
+
+
+const data = [
+  {
+    id: 1,
+    Image: IMG1,
+    title: 'charts templates & infographics in figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/alien_pixels'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Charts templates & infographics in figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/alien_pixels'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Figma dashboard UI kit for data design web apps',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/alien_pixels'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Maintaining tasks and tracking progress',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/alien_pixels'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Charts templates & infographics in figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/alien_pixels'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Charts templates & infographics in figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/alien_pixels'
+  }
+]
+
 
 
 const portfolio = () => {
@@ -9,12 +64,22 @@ const portfolio = () => {
       <h2>portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image"></div>
-          <h3>This is a portfolio item title</h3>
-          <a href='https://github.com' className='btn'>Github</a>
-          <a href='https://dribbble.com/alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
-        </article>
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn'>Github</a>
+                  <a href={demo} className='btn btn-primary' rel="noreferrer" target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
