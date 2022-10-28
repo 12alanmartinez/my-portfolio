b0d7d072aef8b1ea419c6813d2a7e30200336615
alan marinez <alanmartinez319@yahoo.com>
2022-10-28 00:55:25 -0500

    finished the website! now to tweak things and do my copywrite. i may add another option on the navbar.

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {FaCalendarAlt} from 'react-icons/fa'
+import {BsWhatsapp} from 'react-icons/bs'
+import  {useRef} from 'react';
+import emailjs from 'emailjs-com'
 
-const contact = () => {
+const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_s1i9eoc', 'template_hgv63kn', form.current, '74oHQoXJJBjl9kjbQ')
+       
+      e.target.reset()
+    };
+
+  
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <a href='mailto:12alanmartinez@gmail.com' target='_blank' rel="noreferrer" >Send a message</a>
+          </article>
+          <article className="contact__option">
+            <FaCalendarAlt className='contact__option-icon'/>
+            <h4>Calendly</h4>
+            <a href='https://calendly.com/12alanmartinez/30min' target='_blank'  rel="noreferrer">Book with me </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <a href='https://api.whatsapp.com/send?phone=6822343778' target="_blank" rel="noreferrer">Send me a text </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='sumbit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
-export default contact
+export default Contact
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import './footer.css'
+import {AiFillLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import { useState } from 'react'
 
 
 const footer = () => {
+  
   return (
-    <section id='footer'>Footer</section>
+    <footer>
+      <a href='#' className='footer__logo'>ALAN</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials' >Testimonials</a></li>
+        <li><a href='#contact' >Contact</a></li>
+      </ul>
+     
+      <div className="footer__socials">
+        <a href='https://www.linkedin.com/in/alan-martinez-871202228/'> <AiFillLinkedin/> </a>
+        <a href='https://github.com/Cozylillowcas'> <AiFillGithub/> </a>
+        <a href='https://twitter.com/AlanLovesToCode'> <AiOutlineTwitter/> </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Alan Martinez. ALL rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
